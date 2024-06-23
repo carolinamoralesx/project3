0ade95538a2c54dcd4a469ded6cc5dfc6f1f73a2
carolinamoralesx <113265941+carolinamoralesx@users.noreply.github.com>
2024-06-23 14:12:19 -0500

    Update and rename Hola.js to SpeakGoodBye.js

--- Hola.js
+++
@@ -1,9 +0,0 @@
-(function(ventana){
-    var hola = {};
-    var hablapalabra = "Hello";
-    hola.speak = function (name) {
-      console.log(hablapalabra + " " + name);
-    }
-    ventana.hola = hola;
-  }
-)(ventana);
