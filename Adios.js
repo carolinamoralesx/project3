7f6a2f61c2b1bae5571c1fce7138f7ebbeba2be7
carolinamoralesx <113265941+carolinamoralesx@users.noreply.github.com>
2024-06-23 14:12:39 -0500

    Update and rename Adios.js to SpeakHello.js

--- Adios.js
+++
@@ -1,8 +0,0 @@
-(function(ventana) {
-  var adios ={};
-  var hablapalabra = "Good Bye";
-  adios.speak = function(name) {
-    console.log(hablapalabra + " " + name);
-  }
-  ventana.adios = adios;
-})(ventana);
