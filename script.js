e3b4af982534288ef4fc26dd6a395b88fbee4f7d
carolinamoralesx <113265941+carolinamoralesx@users.noreply.github.com>
2024-06-23 14:09:32 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+(
+  function(){
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for ( var i = 0; i < names.length; i++) {
+      var firstLetter =names[i].charAt(0);
+
+      if (firstLetter.toLocaleLowerCase() == "j") {
+        hablapalabra.speak(names[i]);
+      } else {
+        hablapalabra.speak(names[i]);
+      }
+    }
+  }
+)();
